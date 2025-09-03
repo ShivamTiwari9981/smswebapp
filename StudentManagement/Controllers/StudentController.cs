4da07422653b887d2f8d8ac35a875d5e5242875f
Shivam Tiwari <shivamtiwari8756@gmail.com>
2025-09-03 22:31:31 +0530

    first commit

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ILogger<StudentController> _logger;
+        private readonly List<StudentViewModel> _students = new List<StudentViewModel>
+        {
+            new StudentViewModel { Id = 1, FullName = "Alice Johnson", Email = "alice@gmail.com"}
+        };
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            
+            return View(_students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StudentManagement/Models/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagement.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
